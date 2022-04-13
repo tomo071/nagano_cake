4d024a22bfd5ae8a3edffb97e74293bd5c4f9130
EC2 Default User <tomofun6@gmail.com>
2022-04-13 07:01:46 +0000

    [add]各コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/ragistrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
